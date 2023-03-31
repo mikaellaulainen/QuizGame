bbe3014fba2fdb47dc7e15e3d35a6d6ba8b8bb99
mikaellaulainen <laulainenmikael@gmail.com>
2023-03-31 14:32:02 +0300

    Started building question creator

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,6 +1,18 @@
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
 const Create = () => {
+  //Creating new quiz here
   return (
-    <div>Create</div>
+    <div>
+      <Form className='border border-dark col-6 mx-auto my-3 p-3'>
+        <Form.Label>Create new Quiz</Form.Label>
+        <Form.Control type='text' placeholder='Quiz name'/>
+        <Form.Label>First question</Form.Label>
+        <Form.Control type='text' placeholder='Quiz name'/>
+        <Form.Control type='text' placeholder='answer option 1'/>
+        <Button className='my-3'>Create</Button>
+      </Form>
+    </div>
   )
 }
 
