6935d17b373ca01ff32aaa27e5b1c67a7e0fb4fd
mikaellaulainen <laulainenmikael@gmail.com>
2023-04-04 13:26:53 +0300

    Added possibility to add user to database

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -2,3 +2,25 @@ const bcrypt = require('bcrypt') //THIS LIBRARY IS USED TO CRYPT PASSWORD
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+usersRouter.post('/',async (req,res) => {
+  const { username,fullname,password } = req.body
+
+  const passwordHash =await bcrypt.hash(password,10)
+
+  const  user = new User({
+    username,
+    fullname,
+    passwordHash
+  })
+
+  const savedUser = await user.save()
+
+  res.status(201).json(savedUser)
+})
+
+usersRouter.get('/',async (req,res) => {
+  const users = await User.find({})
+  res.json(users)
+})
+
+module.exports = usersRouter
