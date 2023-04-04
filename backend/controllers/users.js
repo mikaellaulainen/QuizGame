dc87c5d0efcaf5b7ae15f94dd30ae52364afaf9d
mikaellaulainen <laulainenmikael@gmail.com>
2023-04-04 13:13:27 +0300

    Started building user controlling

---
+++ backend/controllers/users.js
@@ -0,0 +1,4 @@
+const bcrypt = require('bcrypt') //THIS LIBRARY IS USED TO CRYPT PASSWORD
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
---
+++ backend/models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  passwordHash: {
+    type: String,
+    required: true
+  },
+  fullname: {
+    type: String,
+    required: true
+  }
+})
+
+userSchema.set('toJSON', {
+  transform: (document,returnedObject) => {
+    delete returnedObject.passwordHash
+  }
+})
+
+module.exports = mongoose.model('User', userSchema)
