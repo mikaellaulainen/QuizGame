64012b67636f8f29449a555d7dbf95921d902374
mikaellaulainen <laulainenmikael@gmail.com>
2023-04-05 12:13:47 +0300

    Made login backend to work (mvp)

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -20,7 +20,7 @@ loginRouter.post('/', async (req,res) => {
     id: user._id
   }
 
-  const token = jwt.sign(userForToken, process.env.SECRET_TOKEN)
+  const token = jwt.sign(userForToken, process.env.SECRET_TOKEN, { expiresIn: 60 }) //Token expires in 1 minute! ADJUST THIS WHEN LAUNCHING
   res.status(200).send({ token, username: user.username, fullname: user.fullname })
 })
 
--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -9,16 +9,24 @@ const requestLogger = (req,res,next) => {
 }
 
 const unknownEndpoint = (req,res) => {
-  res.status(404).send({ error: 'UNKNOWN ENDPOIJNT' })
+  res.status(404).send({ error: 'UNKNOWN ENDPOINT' })
 }
 
 const errorHandler = (error,req,res,next) => {
   logger.error(error.message)
 
   if(error.name === 'Casterror') {
-    return res.status(400).send({ error : 'malformatted id' })
+    return res.status(400).send({ error : 'MALFORMATTED ID' })
   } else if(error.name === 'ValidationError'){
     return res.status(400).json({ error : error.message })
+  } else if (error.name === 'JsonWebTokenError') {
+    return res.status(401).json({
+      error: 'INVALID TOKEN'
+    })
+  } else if (error.name === 'TokenExpiredError') {
+    return res.status(401).json({
+      error: 'TOKEN EXPIRED'
+    })
   }
   next(error)
 }
