2247ad24748fdf25803c5501cec3174f5de0d226
mikaellaulainen <laulainenmikael@gmail.com>
2023-04-05 13:36:23 +0300

    Added navigating back,if not logged in

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,10 +1,14 @@
 import { Button } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
+import { Navigate } from 'react-router-dom'
 
-const Game = ({ logout }) => {
-
+const Game = ({ user,logout }) => {
+  if(!user){
+    return <Navigate to='/'/>
+  }
   return (
     <div className="game">
+      <p>Welcome {user.fullname}!</p>
       <p>Here we should have quiz options,and possibility to make own quiz</p>
       <LinkContainer to='/create'>
         <Button>Create your own quiz</Button>
