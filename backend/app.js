6e0a26b1d6573ea473a403ea7f9f9e018f67842b
mikaellaulainen <laulainenmikael@gmail.com>
2023-04-05 12:03:24 +0300

    login returns user info and token

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose =require('mongoose')
+const loginRouter = require('./controllers/login')
 
 mongoose.set('strictQuery', false)
 
@@ -26,6 +27,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/quizzes', quizzesRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
